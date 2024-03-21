216aacdd17f463bd801a3b46d34989b9f65983ec
tony <tony6liu@gmail.com>
2024-03-21 23:45:15 +0800

    constructDB and add new field

--- main.go
+++ main.go
@@ -52,12 +52,13 @@ const (
 )
 
 func init() {
+	DBconnect()
 	DBconstruct()
 }
 
 var db *sql.DB
 
-func DBconstruct() {
+func DBconnect() {
 	// connect to the database
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable",
 		host, port, user, password)
@@ -78,6 +79,24 @@ func DBconstruct() {
 	log.Info("Successfully connected to the database!")
 }
 
+func DBconstruct() {
+	// create the advertisement table
+	createStmt := `
+		CREATE TABLE IF NOT EXISTS advertisement (
+			id SERIAL PRIMARY KEY,
+			title TEXT,
+			start_at TIMESTAMP,
+			end_at TIMESTAMP,
+			conditions JSONB
+		)
+	`
+	_, err := db.Exec(createStmt)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+}
+
 func main() {
 	// create an entry for the advertisement in the database
 	if db == nil {
@@ -86,14 +105,18 @@ func main() {
 	}
 
 	// Insert a new advertisement into the database
+	// conditions is a JSONB type, contain the field like age,gender,country and platform
 
 	insertStmt := `
 		INSERT INTO advertisement (title, start_at, end_at, conditions)
 		VALUES ($1, $2, $3, $4)
 	`
-	_, err := db.Exec(insertStmt, "New Advertisement", "2021-01-01T00:00:00Z", "2021-12-31T23:59:59Z", `{"age": 18, "location": "USA"}`)
+	_, err := db.Exec(insertStmt, "Ad1", "2021-01-01", "2021-12-31", `{"age": 18,
+		"gender": "M", "country": "US", "platform": "ios"}`)
+	
 	if err != nil {
 		log.Error(err)
+		return
 	}
 	
 	defer db.Close()
@@ -106,5 +129,17 @@ func main() {
 	}
 	defer rows.Close()
 
-	log.Info("id | title | start_at | end_at | conditions")
+	for rows.Next() {
+		var id int
+		var title string
+		var startAt string
+		var endAt string
+		var conditions string
+		err = rows.Scan(&id, &title, &startAt, &endAt, &conditions)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		log.Info(title, startAt, endAt, conditions)
+	}
 }
